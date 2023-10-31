5a753da75c8b8e18c6cd9b7bd30ee1e44dd98959
renata-aline <renatalinec@gmail.com>
2023-10-31 19:22:39 -0300

    Formulário de cadastro do projeto mais bela

---
+++ src/Componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return (
+        <header className='banner'>
+            <img src="/imagens/logo-banner.jpeg" alt="Banner principal da página "/>
+            <h1>Mais bela</h1>
+        </header>
+    )
+}
+
+export default Banner
